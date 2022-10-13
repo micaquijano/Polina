4e265545b61dcb75ec337629e8696646757b0d13
micaela quijano <micaquijano04@gmail.com>
2022-10-12 21:51:39 -0300

    categorias

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes,  BrowserRouter, Route, } from 'react-router-dom';
 import { Contactos } from './Contactos';
+import NuestrosProductos from './NuestrosProductos';
 
 export const Main = () => {
   return (
@@ -8,7 +9,8 @@ export const Main = () => {
 <BrowserRouter>
 <Routes>
 <Route exact path="/Contactos" element={<Contactos/>}/>
-  </Routes> 
+<Route exact path="/id:NuestrosProductos" element={<NuestrosProductos/>}/>
+</Routes> 
   </BrowserRouter>
 </>
   )
--- src/NavBar.js
+++ src/NavBar.js
@@ -16,11 +16,11 @@ export const NavBar = () => {
             <Nav.Link className='buttons' href="#SobreNosotros">Sobre Nosotros</Nav.Link>
             <Nav.Link className='buttons' href="/Contactos">Contactos</Nav.Link>
             <NavDropdown className='buttons' title="Nuestros Productos" id="collasible-nav-dropdown">
-              <NavDropdown.Item className='buttons' href="#action/3.1">Tazas Personalizadas</NavDropdown.Item>
-              <NavDropdown.Item className='buttons' href="#action/3.2">
+              <NavDropdown.Item className='buttons' href="/NuestrosProductos/1">Tazas Personalizadas</NavDropdown.Item>
+              <NavDropdown.Item className='buttons' href="/NuestrosProductos/2">
               Tazas en Stock
               </NavDropdown.Item>
-              <NavDropdown.Item className='buttons' href="#action/3.3">Deco</NavDropdown.Item>
+              <NavDropdown.Item className='buttons' href="/NuestrosProductos/3">Deco</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
